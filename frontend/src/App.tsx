9033841a68f89aacbc2c373755a9df392b5c2dbc
eureu <venekbespalov@mail.ru>
2024-11-23 00:40:08 +0300

    upd: props url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,34 @@
+import { Button, Input } from "antd";
 import UploadButton from "./UploadButton";
 import OpenSeadragonViewer from "./Viewer";
+import { useState } from "react";
 
 function App() {
   // const tileSource =
-  //   "https://openseadragon.github.io/example-images/duomo/duomo.dzi";
-  const tileSource = {
-    type: "image",
-    url: "https://img.goodfon.ru/wallpaper/nbig/a/69/kartinka-3d-dikaya-koshka.webp",
+  // "https://openseadragon.github.io/example-images/duomo/duomo.dzi";
+  const [tileSource, setTileSource] = useState(
+    "https://openseadragon.github.io/example-images/duomo/duomo.dzi"
+  );
+  // "https://img.goodfon.ru/wallpaper/nbig/a/69/kartinka-3d-dikaya-koshka.webp"
+  const [inputValue, setInputValue] = useState("");
+  // const tileSource = {
+  //   type: "image",
+  //   url: "https://img.goodfon.ru/wallpaper/nbig/a/69/kartinka-3d-dikaya-koshka.webp",
+  // };
+
+  const handleChange = () => {
+    setTileSource(inputValue);
   };
+
   return (
     <div>
+      <Input
+        onChange={(e) => setInputValue(e.target.value)}
+        defaultValue="Combine input and button"
+      />
+      <Button onClick={handleChange} type="primary">
+        Submit
+      </Button>
       <OpenSeadragonViewer tileSource={tileSource} />
       <UploadButton />
     </div>
