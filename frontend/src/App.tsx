f207237f959b93581aa2b835db4b09cdcb513710
eureu <venekbespalov@mail.ru>
2024-11-24 12:41:48 +0300

    upd: sidebar bug fixies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,8 @@ import { Button, Input } from "antd";
 
 import OpenSeadragonViewer from "./Viewer";
 import { useState } from "react";
-import { Sidebar } from "./widgets/Sidebar";
+import { Sidebar } from "./widgets/Sidebar/Sidebar";
+import Menu from "./widgets/Menu/Menu";
 
 function App() {
   // const tileSource =
@@ -21,19 +22,53 @@ function App() {
     setTileSource(inputValue);
   };
 
+  const [backendData, setBackendData] = useState(null);
+
+  const handleDataFromBackend = (data) => {
+    console.log("Received data:", data);
+    setBackendData(data); // Обновляем состояние
+  };
+
+  console.log(backendData);
+
+  const [flag, setFlag] = useState(false);
+  const handleDataToBackend = () => {
+    setFlag(true);
+  };
+
+  const [coords, setCoords] = useState();
+  const handlerCoords = (data) => {
+    setCoords(data);
+  };
+
   return (
     <div>
-      <OpenSeadragonViewer tileSource={tileSource} />
-      {/* <div id="minimap-navigator"></div> */}
+      <OpenSeadragonViewer
+        tileSource={
+          backendData
+            ? "http://93.183.82.224:3000/images/download-file/" +
+              backendData.dziKey
+            : "https://openseadragon.github.io/example-images/duomo/duomo.dzi"
+        }
+        zoom={backendData ? backendData.viewportZoom : 0}
+        x={backendData ? backendData.viewportX : 0}
+        y={backendData ? backendData.viewportY : 0}
+        flag={flag}
+        handlerCoords={handlerCoords}
+      />
+      <Menu
+        handleDataFromBackend={handleDataFromBackend}
+        handleDataToBackend={handleDataToBackend}
+        coords={coords}
+      />
 
-      <Input
+      {/* <Input
         onChange={(e) => setInputValue(e.target.value)}
         defaultValue="Combine input and button"
       />
       <Button onClick={handleChange} type="primary">
         Submit
-      </Button>
-      <Sidebar />
+      </Button> */}
     </div>
   );
 }
--- frontend/src/Viewer.tsx
+++ frontend/src/Viewer.tsx
@@ -1,15 +1,19 @@
 import { useEffect, useRef } from "react";
 import OpenSeadragon from "openseadragon";
 import styles from "./Viewer.module.scss";
+import axios from "axios";
 
 type TileType = {
   tileSource?: string;
+  zoom?: number;
+  x?: number;
+  y?: number;
+  flag?: boolean;
 };
 
 const OpenSeadragonViewer = (props: TileType) => {
-  const { tileSource } = props;
+  const { tileSource, zoom, x, y, flag, handlerCoords } = props;
   const viewerRef = useRef<HTMLDivElement | null>(null); // Указываем, что это div
-  const sidebarRef = useRef<HTMLDivElement | null>(null);
   const viewerInstanceRef = useRef<OpenSeadragon.Viewer | null>(null);
   const navigatorRef = useRef<HTMLDivElement>(null);
 
@@ -21,105 +25,185 @@ const OpenSeadragonViewer = (props: TileType) => {
         element: viewerRef.current, // Указываем DOM-элемент
         tileSources: tileSource,
         showNavigator: true,
+        navigatorAutoFade: false,
         defaultZoomLevel: 1,
-        prefixUrl: "./src/assets/icons/",
-        navImages: {
-          zoomIn: {
-            REST: "zoomin_rest.svg",
-            GROUP: "zoomin_hover.svg",
-            HOVER: "zoomin_hover.svg",
-            DOWN: "zoomin_down.svg",
-          },
-          zoomOut: {
-            REST: "zoom_out_rest.svg",
-            GROUP: "zoom_out_hover.svg",
-            HOVER: "zoom_out_hover.svg",
-            DOWN: "zoom_out_down.svg",
-          },
-          home: {
-            REST: "home_rest.svg",
-            GROUP: "home_hover.svg",
-            HOVER: "home_hover.svg",
-            DOWN: "home_down.svg",
-          },
-          fullpage: {
-            REST: "fullpage_rest.svg",
-            GROUP: "fullpage_hover.svg",
-            HOVER: "fullpage_hover.svg",
-            DOWN: "fullpage_down.svg",
-          },
-        },
-        navigatorContainer: navigatorRef.current,
+        // toolbar: "toolbar-container",
+        // navigatorContainer: navigatorRef.current,
+        navigatorContainer: "navigator-container",
+        // prefixUrl: "./src/assets/icons/",
+        // navImages: {
+        //   zoomIn: {
+        //     REST: "zoomin_rest.svg",
+        //     GROUP: "zoomin_hover.svg",
+        //     HOVER: "zoomin_hover.svg",
+        //     DOWN: "zoomin_down.svg",
+        //   },
+        //   zoomOut: {
+        //     REST: "zoom_out_rest.svg",
+        //     GROUP: "zoom_out_hover.svg",
+        //     HOVER: "zoom_out_hover.svg",
+        //     DOWN: "zoom_out_down.svg",
+        //   },
+        //   home: {
+        //     REST: "home_rest.svg",
+        //     GROUP: "home_hover.svg",
+        //     HOVER: "home_hover.svg",
+        //     DOWN: "home_down.svg",
+        //   },
+        //   fullpage: {
+        //     REST: "fullpage_rest.svg",
+        //     GROUP: "fullpage_hover.svg",
+        //     HOVER: "fullpage_hover.svg",
+        //     DOWN: "fullpage_down.svg",
+        //   },
+        // },
       });
     } else if (viewerInstanceRef.current) {
-      // Если Viewer уже инициализирован, обновляем его source
+      //       // Если Viewer уже инициализирован, обновляем его source
       viewerInstanceRef.current.open(tileSource);
     }
 
-    // const viewer = OpenSeadragon({
-    //   // id: "openseadragon-viewer", // ID элемента DOM для рендера
-    //   id: viewerRef.current.id,
-    //   prefixUrl:
-    //     // "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.0.0/images/",
-    //     "./src/assets/icons/",
-    //   // navigatorId: "minimap-navigator",
-    //   // navigatorId: minimapRef.current.id,
-    //   tileSources: tileSource,
-    //   showNavigator: true,
-    //   defaultZoomLevel: 1,
-    //   // navigatorAutoResize: false,
-    //   gestureSettingsMouse: {
-    //     clickToZoom: true,
-    //     scrollToZoom: true,
-    //   },
-    //   opacity: 1, // прозрачность
-    //   showReferenceStrip: true,
-    //   navigationControlAnchor: 3,
-    // navImages: {
-    //   zoomIn: {
-    //     REST: "zoomin_rest.svg",
-    //     GROUP: "zoomin_hover.svg",
-    //     HOVER: "zoomin_hover.svg",
-    //     DOWN: "zoomin_down.svg",
-    //   },
-    //   zoomOut: {
-    //     REST: "zoom_out_rest.svg",
-    //     GROUP: "zoom_out_hover.svg",
-    //     HOVER: "zoom_out_hover.svg",
-    //     DOWN: "zoom_out_down.svg",
-    //   },
-    //   home: {
-    //     REST: "home_rest.svg",
-    //     GROUP: "home_hover.svg",
-    //     HOVER: "home_hover.svg",
-    //     DOWN: "home_down.svg",
-    //   },
-    //   fullpage: {
-    //     REST: "fullpage_rest.svg",
-    //     GROUP: "fullpage_hover.svg",
-    //     HOVER: "fullpage_hover.svg",
-    //     DOWN: "fullpage_down.svg",
-    //   },
-    // },
-    //   // collectionMode: true,
+    //     // const viewer = OpenSeadragon({
+    //     //   // id: "openseadragon-viewer", // ID элемента DOM для рендера
+    //     //   id: viewerRef.current.id,
+    //     //   prefixUrl:
+    //     //     // "https://cdnjs.cloudflare.com/ajax/libs/openseadragon/4.0.0/images/",
+    //     //     "./src/assets/icons/",
+    //     //   // navigatorId: "minimap-navigator",
+    //     //   // navigatorId: minimapRef.current.id,
+    //     //   tileSources: tileSource,
+    //     //   showNavigator: true,
+    //     //   defaultZoomLevel: 1,
+    //     //   // navigatorAutoResize: false,
+    //     //   gestureSettingsMouse: {
+    //     //     clickToZoom: true,
+    //     //     scrollToZoom: true,
+    //     //   },
+    //     //   opacity: 1, // прозрачность
+    //     //   showReferenceStrip: true,
+    //     //   navigationControlAnchor: 3,
+    //     // navImages: {
+    //     //   zoomIn: {
+    //     //     REST: "zoomin_rest.svg",
+    //     //     GROUP: "zoomin_hover.svg",
+    //     //     HOVER: "zoomin_hover.svg",
+    //     //     DOWN: "zoomin_down.svg",
+    //     //   },
+    //     //   zoomOut: {
+    //     //     REST: "zoom_out_rest.svg",
+    //     //     GROUP: "zoom_out_hover.svg",
+    //     //     HOVER: "zoom_out_hover.svg",
+    //     //     DOWN: "zoom_out_down.svg",
+    //     //   },
+    //     //   home: {
+    //     //     REST: "home_rest.svg",
+    //     //     GROUP: "home_hover.svg",
+    //     //     HOVER: "home_hover.svg",
+    //     //     DOWN: "home_down.svg",
+    //     //   },
+    //     //   fullpage: {
+    //     //     REST: "fullpage_rest.svg",
+    //     //     GROUP: "fullpage_hover.svg",
+    //     //     HOVER: "fullpage_hover.svg",
+    //     //     DOWN: "fullpage_down.svg",
+    //     //   },
+    //     // },
+    //     //   // collectionMode: true,
+    //     // });
+
+    //     // viewer.navigator.element.style.zIndex = "500";
+
+    if (zoom && x && y) {
+      const zoomLevel = zoom; // Желаемый уровень зума
+      const zoomCenter = new OpenSeadragon.Point(x, y); // Координаты в viewport
+
+      // Установить зум с фокусом на точке
+
+      viewerInstanceRef.current.addHandler("open", () => {
+        viewerInstanceRef.current.viewport.zoomTo(
+          zoomLevel,
+          zoomCenter
+          // viewerInstanceRef.current.viewport.imageToViewportCoordinates(
+          //   zoomCenter
+          // )
+        ); // Зум сработает только после загрузки изображения
+      });
+
+      console.log(zoom, x, y);
+    }
+
+    const currentCenter = viewerInstanceRef.current.viewport.getCenter();
+    // const imagePoint =
+    //   viewerInstanceRef.current.viewport.viewportToImageCoordinates(
+    //     currentCenter
+    //   );
+    // console.log(
+    //   "Current Center (image coordinates):",
+    //   imagePoint.x,
+    //   currentCenter
+    // );
+
+    // useEffect(() => {
+    if (flag) {
+      const currentZoom = viewerInstanceRef.current.viewport.getZoom();
+      const requestData = {
+        'x': currentCenter.x,
+        'y': currentCenter.y,
+        'zoom': currentZoom,
+        'dziKey': tileSource,
+        'originalName':
+      };
+      handlerCoords(requestData);
+    }
+    // console.log("Current Zoom Level:", currentZoom);
+    // const sendTo = async () => {
+    //   try {
+    // const requestData = {
+    //   x: currentCenter.x,
+    //   y: currentCenter.y,
+    //   zoom: currentZoom,
+    //   dziKey: tileSource,
+    // };
+    //     const response = await axios.post(
+    //       "http://93.183.82.224:3000/images/create-screenshot",
+    //       requestData
+    //     );
+    //     console.log("Response data:", response.data);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // };
+    // }, [flag]);
+
+    // viewerInstanceRef.current.addHandler("open", () => {
+    //   viewerInstanceRef.current.viewport.zoomTo(2.0); // Зум сработает только после загрузки изображения
     // });
 
-    // viewer.navigator.element.style.zIndex = "500";
+    viewerInstanceRef.current.addHandler("canvas-click", function (event) {
+      const viewportPoint = viewerInstanceRef.current.viewport.pointFromPixel(
+        event.position
+      );
+      const imagePoint =
+        viewerInstanceRef.current.viewport.viewportToImageCoordinates(
+          viewportPoint
+        );
+      addMarker(imagePoint);
+    });
 
     return () => {
       viewerInstanceRef.current?.destroy();
       viewerInstanceRef.current = null;
     };
 
-    // const navigatorElement = viewer.navigator.element.id;
-    // console.log(navigatorElement);
+    //     // const navigatorElement = viewer.navigator.element.id;
+    //     // console.log(navigatorElement);
   }, [tileSource]);
-  // }, []);
+  //   // }, []);
 
-  // useEffect(() => {
-  //   console.log("viewerRef:", viewerRef.current);
-  //   console.log("minimapRef:", minimapRef.current.id);
-  // });
+  //   // useEffect(() => {
+  //   //   console.log("viewerRef:", viewerRef.current);
+  //   //   console.log("minimapRef:", minimapRef.current.id);
+  //   // });
 
   return (
     <div className={styles.viewerContainer}>
@@ -128,58 +212,61 @@ const OpenSeadragonViewer = (props: TileType) => {
         <div className={styles.sidebar}>
           <div ref={navigatorRef} className={styles.navigator}></div>{" "}
           {/* Контейнер для миникарты */}
-          <button>+</button>
+          {/* <button>+</button>
           <button>-</button>
           <div className={styles.zoomInfo}>1.0x</div>
           <button>⬆️</button>
-          <button>⤢</button>
+          <button>⤢</button> */}
+          {/* <div id="toolbar-container"></div> */}
           <hr />
-          <div className={styles.actions}>
+          {/* <div className={styles.actions}>
             <div>Комментарий</div>
             <div>Скриншот</div>
             <div>Нейросеть</div>
             <div className={styles.curveInfo}>Кривые 376 x 0</div>
             <div className={styles.curveGraph}></div>
-          </div>
+          </div> */}
         </div>
       </div>
+      <div className={styles.menu}></div>
     </div>
-
-    // <div className={styles.viewerContainer}>
-    //   <div ref={viewerRef} className={styles.openseadragonCanvas}></div>
-    //   <div className={styles.viewerOverlay}>
-    //     <div className={styles.sidebar}>
-    //       <button>+</button>
-    //       <button>-</button>
-    //       <div className={styles.zoomInfo}>1.0x</div>
-    //       <button>⬆️</button>
-    //       <button>⤢</button>
-    //       <hr />
-    //       <div className={styles.actions}>
-    //         <div>Комментарий</div>
-    //         <div>Скриншот</div>
-    //         <div>Нейросеть</div>
-    //         <div className={styles.curveInfo}>Кривые 376 x 0</div>
-    //         <div className={styles.curveGraph}></div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
-
-    // <div
-    //   id="openseadragon-viewer"
-    //   ref={viewerRef}
-    //   style={{ width: "100%", height: "600px", border: "1px solid black" }}
-    // ></div>
-
-    // <div className="viewer-container">
-    //   <div id="openseadragon-viewer" ref={viewerRef}></div>
-    //   <div className="sidebar">
-    //     <h3>Сайдбар</h3>
-    //     <p>Контент сайдбара</p>
-    //   </div>
-    // </div>
   );
+
+  //     // <div className={styles.viewerContainer}>
+  //     //   <div ref={viewerRef} className={styles.openseadragonCanvas}></div>
+  //     //   <div className={styles.viewerOverlay}>
+  //     //     <div className={styles.sidebar}>
+  //     //       <button>+</button>
+  //     //       <button>-</button>
+  //     //       <div className={styles.zoomInfo}>1.0x</div>
+  //     //       <button>⬆️</button>
+  //     //       <button>⤢</button>
+  //     //       <hr />
+  //     //       <div className={styles.actions}>
+  //     //         <div>Комментарий</div>
+  //     //         <div>Скриншот</div>
+  //     //         <div>Нейросеть</div>
+  //     //         <div className={styles.curveInfo}>Кривые 376 x 0</div>
+  //     //         <div className={styles.curveGraph}></div>
+  //     //       </div>
+  //     //     </div>
+  //     //   </div>
+  //     // </div>
+
+  //     // <div
+  //     //   id="openseadragon-viewer"
+  //     //   ref={viewerRef}
+  //     //   style={{ width: "100%", height: "600px", border: "1px solid black" }}
+  //     // ></div>
+
+  //     // <div className="viewer-container">
+  //     //   <div id="openseadragon-viewer" ref={viewerRef}></div>
+  //     //   <div className="sidebar">
+  //     //     <h3>Сайдбар</h3>
+  //     //     <p>Контент сайдбара</p>
+  //     //   </div>
+  //     // </div>
+  //   );
 };
 
 export default OpenSeadragonViewer;
